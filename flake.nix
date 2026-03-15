2f81557162dae9a304531936c2b674716dbe78f6
Omar Jatoi <git@omarjatoi.com>
2026-03-15 09:45:29 -0400

    Add nix flake with devShell

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Buck2 rules for Terraform";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [
+            "terraform"
+          ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            buck2
+            buildifier
+            terraform
+          ];
+        };
+      }
+    );
+}
